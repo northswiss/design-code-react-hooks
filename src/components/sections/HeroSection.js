15592e8fcdf48427a5b5b357882ca196ecbfdf5e
Chris Roy <chris@northswiss.com>
2021-08-29 10:38:24 +0200

    Add filter bg and screenshots

--- src/components/sections/HeroSection.js
+++ src/components/sections/HeroSection.js
@@ -4,6 +4,7 @@ import { H1, MediumText } from "../styles/TextStyles"
 import { themes } from "../styles/ColorStyles"
 import PurchaseButton from "../Buttons/PurchaseButton"
 import { Link } from "gatsby"
+import MockupAnimation from "../animations/MockupAnimation"
 
 function HeroSection() {
   return (
@@ -25,6 +26,7 @@ function HeroSection() {
             />
           </Link>
         </TextWrapper>
+        <MockupAnimation />
       </ContentWrapper>
     </Wrapper>
   )
@@ -34,12 +36,15 @@ export default HeroSection
 
 const Wrapper = styled.div`
   background: linear-gradient(180deg, #4316db 0%, #9076e7 100%);
+  overflow: hidden;
 `
 
 const ContentWrapper = styled.div`
   max-width: 1234px;
   margin: 0 auto;
   padding: 200px 30px;
+  display: grid;
+  grid-template-columns: 360px auto;
 `
 
 const TextWrapper = styled.div`
